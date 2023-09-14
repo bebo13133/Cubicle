25ee4a4761d25efab1a32a33776471e9ebbe6a35
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-09-14 11:28:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
-const {log} = require('console')
+const { log } = require('console')
 const path = require('path');
 //const config = require('./config/config');//
 // Express static configuration
 
-app.use(express.static(path.resolve(__dirname,'static')))
+app.use(express.static(path.resolve(__dirname, 'static')))
 
 //Handlebars config
 
-app.engine('hbs',handlebars.engine({
+app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 
 }));
@@ -19,10 +19,10 @@ app.set('view engine', 'hbs')
 app.set('views', 'src/views');
 
 //routes 
-const PORT=5000
+const PORT = 5000
 app.get('/', (req, res) => {
     res.render('index')
 
 })
 
-app.listen(PORT, ()=> log(`Server running on ${PORT}`))
+app.listen(PORT, () => log(`Server running on ${PORT}`))
