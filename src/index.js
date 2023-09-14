43819cea43e3817d8b5085f61c50060604badc56
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-09-14 13:00:16 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const { log } = require('console')
-
+const homeController = require('./controllers/homeController')
 //const config = require('./config/config');//
+
+const PORT = 5000
 require('./config/express')(app);
-require('./config/routes')(app)
+// require('./config/routes')(app)
 
-
+app.use(homeController)
 
 
 //routes 
+app.listen(PORT, () => log(`Server running on ${PORT}`))
