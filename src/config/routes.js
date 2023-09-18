65d4566c88c9bcd680f92af373a3b79f56b426d2
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-09-18 12:13:42 +0300

    add register page

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,12 +4,16 @@ const { log } = require('console')
 const homeController = require('../controllers/homeController')
 const cubeController = require('../controllers/cubeController')
 const accessoryController = require('../controllers/accessoryController')
+const userController = require('../controllers/userController')
+
 
  module.exports = (app) => {
 //    const PORT = 5000
 app.use(homeController)
 app.use("/cubes",cubeController)
 app.use("/accessories",accessoryController)
+app.use("/users",userController)
+
 app.get('*', (req, res) => {
     res.redirect('/404')
 })
