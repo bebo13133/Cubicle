2c5f48580252ab671abc4e6ead8b2124cba57f77
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-09-22 18:29:26 +0300

    add error Handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,35 +3,42 @@ const userService = require('../services/userService')
 const User = require('../models/User')
 
 
-router.get('/register', (req,res) => {
-res.render('./users/register');
+router.get('/register', (req, res) => {
+    res.render('./users/register');
 });
 
-router.post('/register', async (req,res) => {
-const {username, password, repeatPassword} = req.body
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body
 
-await userService.register({username, password, repeatPassword})
+    try {
+        await userService.register({ username, password, repeatPassword })
 
-res.redirect('/users/login')       // ? да се промени на login страницата - done!
+        res.redirect('/users/login')       // ? да се промени на login страницата - done!
+    } catch (err) {
+        const errorMessages = Object.values(err.errors).map(err => err.message)
+
+        res.status(400).render('./users/register',{errorMessages: errorMessages})
+    }
+
 
 });
 
 router.get('/login', (req, res) => {
     res.render('./users/login')
 });
-router.post('/login', async (req,res)=>{
-const {username, password} = req.body
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body
 
 
-const token = await userService.login(username, password)
+    const token = await userService.login(username, password)
 
-res.cookie('auth', token,{httpOnly: true})
+    res.cookie('auth', token, { httpOnly: true })
 
-res.redirect("/")
+    res.redirect("/")
 })
 
 router.get('/logout', (req, res) => {
-    res.clearCookie('auth')    
+    res.clearCookie('auth')
     res.redirect("/")
 
 })
